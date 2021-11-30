16bf88dfdb8f501461cee6ee08b14ba94e9f2701
Hellerin <hellerinm@gmail.com>
2021-11-30 16:18:56 +0200

    added delete single task function

--- app.js
+++ app.js
@@ -4,6 +4,18 @@
 const form = document.querySelector("form")
 form.addEventListener("submit", addTask)
 
+// Task list
+const taskList = document.querySelector("ul");
+taskList.addEventListener("click", delTask)
+
+//delTask
+function delTask(event){
+    if(event.target.textContent === "x"){
+        if(confirm("Do you really want to delete this task?")){
+            event.target.parentElement.remove()
+        }
+    }
+}
 //addTask function
 function addTask(event){
     // get task value from form input
@@ -17,6 +29,16 @@ function addTask(event){
     // add text to element
     const text = document.createTextNode(task)
     li.appendChild(text)
+    // create link
+    const link = document.createElement("a")
+    // add css style
+    link.className = "secondary-content"
+    // add text
+    link.appendChild(document.createTextNode("x"))
+    // add href attribute
+    link.setAttribute("href", "#")
+    // add link to list items
+    li.appendChild(link)
     // add li to taskList
    taskList.appendChild(li)
     // clear form input value
